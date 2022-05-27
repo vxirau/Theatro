74e27b8b4892c857916a6968b7187cea156379c4
VXGamez <victor.xirau@students.salle.url.edu>
2022-05-28 01:07:07 +0200

    day 2

--- dbs_cleaner.py
+++ dbs_cleaner.py
@@ -1,21 +1,30 @@
 
 import csv
 
-input = "aka_name"
+input = "person_info"
 
-with open("datasets/dbs/"+input+".csv", "r", encoding="ISO-8859-1") as source:
+with open("datasets/dbs-prova/"+input+".csv", "r", encoding="ISO-8859-1") as source:
     reader = csv.reader(source)
     line=0
     print("Open "+input+".csv")
-    with open("datasets/dbs/"+input+"-clean.csv", "w") as result:
+    with open("datasets/dbs-prova/"+input+"-clean.csv", "w") as result:
         writer = csv.writer(result)
         print("Writting "+input+"-clean.csv")
         for r in reader:
             line += 1
-            if r[1] == "md5sum)s":
-                writer.writerow((r[0], r[2], r[3], r[4], r[5], r[6], r[7], r[8]))
+            if len(r) < 6:
+                if len(r) == 5:
+                    writer.writerow((r[0], r[1], r[2], r[3], r[4]))
+                else:
+                    print(r)
             else:
-                writer.writerow((r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7]))
+                if len(r[4]) >= 255:
+                    r[4] = r[4:249]
+                    r[4] += " (...)"
+                if r[1] == "note)s":
+                    writer.writerow((r[0], r[2], r[3], r[4], r[5]))
+                else:
+                    writer.writerow((r[0], r[1], r[2], r[3], r[4]))
 
 
         print("Done :)")
