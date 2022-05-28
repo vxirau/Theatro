edbe0562dc127506a349a76e18fa893198db87ca
VXGamez <victor.xirau@students.salle.url.edu>
2022-05-28 19:08:55 +0200

    csv upload v1

--- dbs_cleaner.py
+++ dbs_cleaner.py
@@ -1,30 +1,18 @@
 
 import csv
 
-input = "person_info"
+input = "movie_keyword"
 
-with open("datasets/dbs-prova/"+input+".csv", "r", encoding="ISO-8859-1") as source:
+with open("datasets/csv/dirty/"+input+".csv", "r", encoding="ISO-8859-1") as source:
     reader = csv.reader(source)
     line=0
     print("Open "+input+".csv")
-    with open("datasets/dbs-prova/"+input+"-clean.csv", "w") as result:
+    with open("datasets/csv/clean/"+input+"-clean.csv", "w") as result:
         writer = csv.writer(result)
         print("Writting "+input+"-clean.csv")
         for r in reader:
             line += 1
-            if len(r) < 6:
-                if len(r) == 5:
-                    writer.writerow((r[0], r[1], r[2], r[3], r[4]))
-                else:
-                    print(r)
-            else:
-                if len(r[4]) >= 255:
-                    r[4] = r[4:249]
-                    r[4] += " (...)"
-                if r[1] == "note)s":
-                    writer.writerow((r[0], r[2], r[3], r[4], r[5]))
-                else:
-                    writer.writerow((r[0], r[1], r[2], r[3], r[4]))
+            writer.writerow((r[0], r[2], r[3]))
 
 
         print("Done :)")
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+//IMPORTS DE GRAPHQL
 // import { graphqlHTTP } from "express-graphql";
 // import { buildSchema } from "graphql";
 const express = require('express');
 var ag = require('@bitnine-oss/ag-driver');
 const app = express();
 
-
+//CODI DE GRAPHQL
 // const schema = buildSchema(`
 //       type Image {
 //         id: Int
@@ -26,14 +27,22 @@ var config = {
     port: 5432
 };
 
+//Descomentar aquesta per fer servir la instància remota
+// var config = {
+//     user: 'victorxirau',
+//     password: '',    //No la he posat pq millor intentar que no estigui a github! Osigui no pusheeu un codi amb la contra, o eso o fem un .env si cal
+//     database: 'imdb',
+//     host: 'theatro.cswglp68us7v.eu-west-2.rds.amazonaws.com',
+//     port: 5432
+// };
+
 const client = new ag.Client(config);
 
 
 
 
 
-//client.end();
-
+//ENDPOINT DE GRAPHQL
 // app.use(
 //   "/movieInfo",
 //   graphqlHTTP({
@@ -44,7 +53,7 @@ const client = new ag.Client(config);
 // );
 
 
-//http://localhost:4000/get-details
+//Posar al postman: PETICIÓ POST A http://localhost:4000/get-details
 
 app.post('/get-details', async (req, res) => {
 
@@ -63,7 +72,7 @@ app.post('/get-details', async (req, res) => {
         "fulfillmentMessages": resposta});
 });
 
-//Listening to our server
+
 app.listen(4000, () => {
   console.log("GraphQL server with Express running on localhost:5000/graphql");
 });
